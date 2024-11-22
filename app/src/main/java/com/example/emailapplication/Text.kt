55e2c6fefd3ecc07c710d33f392703ee816f4cff
sathiya_0809 <priyasathiya139@gmail.com>
2024-11-22 10:08:52 +0530

    initial commit

---
+++ app/src/main/java/com/example/emailapplication/Text.kt
@@ -0,0 +1,18 @@
+package com.example.emailapplication
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
+
+class Text(
+    text: String,
+    fontSize: TextUnit,
+    color: Color,
+    modifier: Modifier,
+    textAlign: TextAlign,
+    fontWeight: FontWeight
+) {
+
+}
